20d8fe446df24004c39fcc5bc3697b77b76dbda0
mayor-ace <mayowa@whogohost.com>
2015-01-13 17:29:24 +0100

    Shopping app improved version

--- js/app.js
+++ js/app.js
@@ -1,116 +1,140 @@
-
-var feedback,
-		listItem, 
-		addButton, 
-		cartedItemsHolder, 
-		boughtItemsHolder;
-		
+var addButton = document.getElementById('add_item'),
+		itemInput = document.getElementById('new_item'),
+		cartedItemsHolder = document.getElementById('cart_items'),
+		boughtItemsHolder = document.getElementById('bought_items');
 
 
-var shoppingList = {
+var createNewShoppingListItem = function(itemString) {
 
-		formElements: function() {
-			var itemInput = document.getElementById('new_item'),
-					itemString = itemInput.value;
-
-
-					feedback = document.getElementById('displayMessage'), 
-					addButton = document.getElementById('add_item'), 
-					cartedItemsHolder = document.getElementById('cart'), 
-					boughtItemsHolder = document.getElementById('bought_items')
-		},
-
-		onReady: function() {
-					shoppingList.formElements();
-					addButton.onclick = shoppingList.addItem;
-
-			var checkBox =  cartedItemsHolder.querySelector("input[type=checkbox]"),
-					checkedBox = boughtItemsHolder.querySelector("input[type=checkbox]"),
-					editButton = cartedItemsHolder.querySelector("button.edit"),
-					deleteButton = cartedItemsHolder.querySelector("button.delete"),
-					removeButton = boughtItemsHolder.querySelector("button.delete");
+		var listItem = document.createElement("li"),
+				checkBox = document.createElement("input");
+				label = document.createElement("label"),
+				editInput = document.createElement("input"),
+				editButton = document.createElement("button"),
+				deleteButton = document.createElement("button");
 
 
-					checkBox.onchange = shoppingList.moveItemsToBought;
-					checkedBox.onchange = shoppingList.moveItemToCart;
-					editButton.onclick = shoppingList.editItem;
-					deleteButton.onclick = shoppingList.deleteItem;
-					removeButton.onclick = shoppingList.deleteItem;
-		},
-
-		createNewShoppingItem: function(itemString) {
-
-			var listItem = document.createElement("li"),
-					checkBox = document.createElement("input"),
-					label = document.createElement("label"),
-					editButton = document.createElement("button"),
-					deleteButton = document.createElement("button");
+				checkBox.type = "checkbox";
+				label.innerHTML = itemString;
+				editInput.type = "text";
+				editButton.innerHTML = "Edit";
+				editButton.className = "edit";
+				deleteButton.innerHTML = "Remove Item";
+				deleteButton.className  = "delete";
 
 
-					checkBox.type = "checkbox";
-					editButton.innerHTML = "Edit Item";
-					editButton.className = "edit";
-					deleteButton.innerHTML = "Remove Item";
-					deleteButton.className = "delete";
+				listItem.appendChild(checkBox);				
+				listItem.appendChild(label);
+				listItem.appendChild(editInput);
+				listItem.appendChild(deleteButton);
+				listItem.appendChild(editButton);
 
 
-					listItem.appendChild(checkBox);
-					listItem.appendChild(label);
-					listItem.appendChild(editButton);
-					listItem.appendChild(deleteButton);
-					listItem.appendChild(removeButton);
 
-					return listItem;
-		},
+		bindShoppingEvents(listItem, itemBought);
 
-		addItem: function() {
-
-					feedback.innerHTML = "Item Successfully Added !";
-
-					var newListItem = shoppingList.createNewShoppingItem("Hello World");
-							cartedItemsHolder.appendChild(newListItem);
-
-		},
-
-		moveItemsToBought: function() {
-					var listItem = this.parentNode;
-					boughtItemsHolder.appendChild(listItem);
-					this.onReady(listItem, moveItemToCart);
-		},
-
-		moveItemToCart: function() {
-					var listItem = this.parentNode;
-					cartedItemsHolder.appendChild(listItem);
-					this.onReady(listItem, moveItemsToBought);
-		},
-
-		editItem: function() {
-					var listItem = this.parentNode;
-					var li = listItem.parentNode;
-					li.removeChild(listItem);
-		},
-
-		deleteItem: function() {
-					var listItem = this.parentNode;
-					var li = listItem.parentNode;
-					li.removeChild(listItem);
-		},
-
-		// bindShoppingEvents: function() {
-		// 	append: function() {
-		// 	for (var i = 0; i < onReady.children.length; i++) {
-		// 			onReady(cartedItemsHolder.children[i], itemsBought );
-		// 	};
-
-		// 	for (var i = 0; i < onReady.children.length; i++) {
-		// 			onReady(boughtItemsHolder.children[i], itemsCarted );
-		// 	}
-
-		// };
-		// }
-		
-
+		return listItem;
 };
 
-window.onload = shoppingList.onReady;
+var addItem = function() {
 
+	var listItem = createNewShoppingListItem(itemInput.value);
+			
+	//Dealing with user input..
+		//for empty strings..
+			if(itemInput.value === "") {
+				label.innerHTML = prompt("You haven't added an item.. Add item here..");
+					if(label.innerHTML === "") {
+						alert("You haven't added any item still.. Click Ok to reload page and try again..");
+						window.location.reload();
+					}
+					else {
+						cartedItemsHolder.appendChild(listItem);
+					}
+			}
+
+			//Appending the newly created list item to the carted items holder
+			else {
+				cartedItemsHolder.appendChild(listItem);
+			}
+
+			itemInput.value = "";
+};
+
+var editItem = function() {
+		console.log('This function EDIT item has been called.. It works.. Items can now be edited');
+		var listItem = this.parentNode;
+				editButton = listItem.querySelector("button.edit")
+				editInput = listItem.querySelector("input[type=text]"),
+				label = listItem.querySelector("label"),
+
+				containsClass = listItem.classList.contains("editMode");
+
+		if(containsClass) {
+			label.style.display = "inline";
+			editInput.style.display = "none";
+			label.innerHTML = editInput.value;
+			editButton.innerHTML = "Edit";
+		} 
+		else {
+			editInput.style.display = "inline";
+			label.style.display = "none";
+			editInput.value = label.innerHTML;
+			listItem.className = "editMode";
+			editButton.innerHTML = "Save";
+			return listItem.className;
+		}
+
+		listItem.classList.toggle("editMode");
+};
+
+var deleteItem = function() {
+		console.log('This function DELETE item has been called.. It works.. Items can now be deleted');
+		var listItem = this.parentNode,
+				ul = listItem.parentNode;
+
+		ul.removeChild(listItem);
+};
+
+var itemCarted = function() {
+		console.log('function called.. itemCarted.. returned from the bought items category..');
+		var listItem = this.parentNode;
+		boughtItemsHolder.appendChild(listItem);
+		bindShoppingEvents(listItem, itemBought);
+};
+
+var itemBought =  function() {
+		console.log('Function called.. itemBought..');
+		var listItem = this.parentNode;
+		cartedItemsHolder.appendChild(listItem);
+		bindShoppingEvents(listItem, itemCarted);
+};
+
+
+//This function will be used to bind events to the list Items children
+var bindShoppingEvents = function(shoppingListItem, checkBoxEventHandler) {
+
+		var checkBox = shoppingListItem.querySelector("input[type=checkbox]"),
+				editButton = shoppingListItem.querySelector("button.edit"),
+				deleteButton = shoppingListItem.querySelector("button.delete");
+
+		console.log("Events have been bound..");
+		
+		checkBox.onchange = checkBoxEventHandler;
+		editButton.onclick = editItem;
+		deleteButton.onclick = deleteItem;
+		
+};
+
+// This function will be used to cycle through the list items in the unordered list called the cartedItemsHolder
+//cycle over the cartedItemsHolder list items
+for(var i = 0; i < cartedItemsHolder.children.length; i+=1) {
+	bindShoppingEvents(cartedItemsHolder.children[i], itemCarted);
+}
+
+//cycle over the boughtItemsHolder list items
+for(var i = 0; i < boughtItemsHolder.children.length; i+=1) {
+	bindShoppingEvents(boughtItemsHolder.children[i], itemBought);
+}
+
+addButton.addEventListener('click', addItem);
